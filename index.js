f5eacc550bf74fce3d81f1499647b49f6f9c2463
Ashique <ashique.mohammed@justride.in>
2017-08-17 15:46:34 +0530

    Pushing changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var express = require('express')
 var request = require('request')
 var bodyParser = require('body-parser')
 var app = express()
-// var urlencodedParser = bodyParser.urlencoded({ extended: false })
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 var firebase = require("firebase");
 var moment = require('moment');
